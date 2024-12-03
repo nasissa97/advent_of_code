b9b994514ec116de5603e3d239aba862af2a9b89
Nas Issa <nasissa97@gmail.com>
2024-12-02 21:48:24 -0800

    Complete Day 2

--- 2024/day2/src/main.rs
+++ 2024/day2/src/main.rs
@@ -40,33 +40,54 @@ fn determine_order(numbers: &[u32]) -> Option<&'static str> {
     }
 }
 
+fn is_safe(report: &[i32]) -> bool {
+    if report.len() < 2 {
+        return true
+    }
+
+    let mut increasing = true;
+    let mut decreasing = true;
+
+    for window in report.windows(2) {
+        let diff = window[1] - window[0];
+        if !(1..=3).contains(&diff) {
+            increasing = false;
+        }
+        if !(-3..=-1).contains(&diff) {
+            decreasing = false
+        }
+    }
+
+    increasing || decreasing
+}
+
+fn check_with_remove(report: &[i32]) -> bool {
+    for i in 0..report.len() {
+        let mut modified_report = report.to_vec();
+        modified_report.remove(i);
+        if is_safe(&modified_report) {
+            return true;
+        }
+    }
+    false
+}
 
 fn main() {
     let file = File::open("data/data1.txt").unwrap();
     let buffer = BufReader::new(file);
-    println!("Hello, world!");
     let mut ans = 0;
     for line in buffer.lines() {
         let value = line.unwrap();
-        let numbers: Vec<u32> = value
+        let numbers: Vec<i32>  = value
             .split_whitespace()
-            .map(|v: &str| v.parse::<u32>().unwrap())
+            .map(|v: &str| v.parse::<i32>().unwrap())
             .collect();
 
-
-        match determine_order(&numbers) {
-            Some("increasing") => {
-                if is_sequence_valid(&numbers, is_increasing) {
-                    ans += 1;
-                }
-            }
-            Some("decreasing") => {
-                if is_sequence_valid(&numbers, is_decreasing) {
-                    ans += 1
-                }
-            }
-            _ => println!("Undefined Order")
+        if is_safe(&numbers) || check_with_remove(&numbers) {
+            ans += 1;
         }
+
+
     }
     println!("Answer: {}", ans);
 }
