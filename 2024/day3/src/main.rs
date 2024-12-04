ce1b7b3b305e76d47f38620554ce23a5d7ae24d0
Nas Issa <nasissa97@gmail.com>
2024-12-04 07:26:21 -0800

    Answer to high

--- 2024/day3/src/main.rs
+++ 2024/day3/src/main.rs
@@ -1,11 +1,17 @@
 use std::fs::File;
 use std::io::{self, BufRead, BufReader};
 
+use core::num::ParseIntError;
 use lazy_regex::regex;
 use thiserror::Error;
-use core::num::ParseIntError;
 
-pub static DAY3_REGEX: &lazy_regex::Lazy<regex::Regex> = regex!(r"mul\(([1-9]{1}\d{0,2}),([1-9]{1}\d{0,2})\)");
+pub static DAY3_REGEX_PART1: &lazy_regex::Lazy<regex::Regex> =
+    regex!(r"mul\(([1-9]{1}\d{0,2}),([1-9]{1}\d{0,2})\)");
+pub static DAY3_REGEX_PART2: &lazy_regex::Lazy<regex::Regex> =
+    regex!(r"(do\(\)|don't\(\)|mul\([1-9][0-9]{0,2},[1-9][0-9]{0,2}\))");
+
+pub static TEST: &lazy_regex::Lazy<regex::Regex> =
+    regex!(r"don't\(\).*?do\(\)|don't\(\).*");
 
 #[derive(Error, Debug)]
 pub enum Day3Error {
@@ -27,55 +33,83 @@ impl Instruction {
     pub fn output(&self) -> u64 {
         self.x * self.y
     }
-    fn parse_mul(input: &str) -> Result<(u64,u64), Day3Error> {
+    fn parse_mul(input: &str) -> Option<Self> {
         let trimmed = &input[4..input.len() - 1];
-        let parts: Result<Vec<u64>, Day3Error> = trimmed
+        let parts: Vec<u64> = trimmed
             .split(",")
-            .into_iter()
-            .map(|num| num.parse::<u64>().map_err(|_| Day3Error::BadInstruction))
+            .filter_map(|num| num.parse::<u64>().ok())
             .collect();
-            
-        let nums = match parts {
-            Ok(nums) => nums,
-            Err(_) => return Err(Day3Error::BadInstruction),
-        };    
-        
-        if nums.len() != 2 {
-            return Err(Day3Error::BadInstruction)
+
+        if parts.len() != 2 {
+            return None
         }
 
-        let num1 = nums.get(0).ok_or(Day3Error::BadInstruction)?.clone();
-        let num2 = nums.get(1).ok_or(Day3Error::BadInstruction)?.clone();
 
-        Ok((num1, num2))
+        Some(Self{x: parts[0], y: parts[1]})
     }
 }
 
 impl TryFrom<String> for Instruction {
     type Error = Day3Error;
     fn try_from(item: String) -> Result<Self, Day3Error> {
-        let (x, y) = Instruction::parse_mul(item.as_str())?;
-        Ok(Self {x, y})
+        let instruction = Instruction::parse_mul(item.as_str());
+        match instruction {
+            Some(instruction) => Ok(instruction),
+            _ => Err(Day3Error::BadInstruction)
+        }
     }
-
 }
 
+fn part1(line: String) -> u64 {
+    let line_value = DAY3_REGEX_PART1
+        .find_iter(&line)
+        .map(|match_| match_.as_str().to_string().try_into())
+        .filter_map(Result::ok)
+        .fold(0, |acc, inst: Instruction| acc + inst.output());
 
-fn main() -> Result<(), Day3Error>{
+    line_value
+}
+
+fn part2(line: String) -> u64 {
+    let mut enabled = true;
+    let mut total = 0;
+
+    for capture in DAY3_REGEX_PART2.find_iter(&line) {
+        let instr = capture.as_str();
+        if instr == "do()" {
+            enabled = true;
+        } else if instr == "don't()" {
+            enabled = false
+        } else if instr.starts_with("mul(") {
+            if enabled {
+                match Instruction::parse_mul(instr) {
+                    Some(instruction) => {
+                        println!("mul enable -> {}", instr);
+                        total += instruction.output()
+                    },
+                    _ => println!("Bad Instruction {:?}", instr),
+                };
+            } else {
+                println!("mul disabled -> Skipped: {}", instr);
+            }
+        }
+    }
+    total
+}
+
+fn main() -> Result<(), Day3Error> {
     println!("Hello, world!");
-    let file = File::open("data/data.txt")?;
+    let file = File::open("data/data2.txt")?;
     let reader = BufReader::new(file);
-    // let mut instructions: Vec<Instruction> = Vec::new();
     let mut ans = 0;
     for line in reader.lines() {
         let value = line?;
-        // println!("{:?}", value);
-        let line_value = DAY3_REGEX.find_iter(&value)
-            .map(|match_| match_.as_str().to_string().try_into())
-            .filter_map(Result::ok)
-            .fold(0, |acc, inst: Instruction| acc + inst.output());
+        let enabled= TEST.replace_all(&value, "").to_string();
+        println!("{}", enabled);
+        let total = part1(enabled);
+        ans += total;
 
-        ans += line_value;
+        // ans += part1(value);
     }
 
     println!("Answer {}", ans);
